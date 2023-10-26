51706cc8ec3527bc80bbc29301324dea7907b687
AddisonWraith <dylandickersonw@gmail.com>
2023-10-26 12:18:07 -0700

    added prompts to take string input for nums

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let num1 = 8
-let num2 = 2
+let num1 = 0
+let num2 = 0
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -10,6 +11,35 @@ document.getElementById("num2-el").textContent = num2
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+function add() {
+    let sum = num1 + num2
+    sumEl.textContent = sum
+}
 
+function subtract() {
+    let sum = num1 - num2
+    sumEl.textContent = sum
+}
 
+function divide() {
+   let sum = num1 / num2
+   sumEl.textContent = sum
+
+}
+
+function multiply() {
+    let sum = num1 * num2
+    sumEl.textContent = sum
+}
+
+function One() {
+    num1 = +prompt("Please enter a value for the first variable: ")
+    document.getElementById("num1-el").textContent = num1
+
+}
+
+function Two() {
+    num2 = +prompt("Please enter a value for the second variable: ")
+    document.getElementById("num2-el").textContent = num2
+}
 
